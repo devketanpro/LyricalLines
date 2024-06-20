65db664d98a7952cc865cd5a19dd78e449020d61
ketandev <devketanpro11@gmail.com>
2024-06-20 12:20:40 +0530

    fetch lyrics from musixmatch via seprate function

--- LyricalLines/settings.py
+++ LyricalLines/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -122,3 +123,6 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+MUSIXMATCH_URL = "https://api.musixmatch.com/ws/1.1/matcher.lyrics.get?"
+MUSIXMATCH_API_KEY = os.getenv("MUSIXMATCH_KEY")
---
+++ lyricist_app/helpers.py
@@ -0,0 +1,43 @@
+import json
+import logging
+import requests
+
+from django.conf import settings
+from requests.exceptions import HTTPError, Timeout, RequestException
+
+logger = logging.getLogger(__name__)
+
+def fetch_lyrics_from_musixmatch(artist, title):
+    """
+    Fetch lyrics from Musixmatch API for a given artist and title.
+
+    Args:
+        artist (str): The name of the artist.
+        title (str): The title of the song.
+
+    Returns:
+        str: The lyrics of the song if found.
+        None: If there was an error during the request or lyrics are not found.
+    """
+
+    musicmatch_url = settings.MUSIXMATCH_URL
+    params = {
+        "q_track": title,
+        "q_artist": artist,
+        "apikey": settings.MUSIXMATCH_API_KEY,
+    }
+    try:
+        response = requests.get(musicmatch_url, params=params, timeout=10)
+        response.raise_for_status()
+        data = response.json()
+        return data.get('message', {}).get("body", {}).get("lyrics", {}).get("lyrics_body")
+    except HTTPError as http_err:
+        logger.error(f"HTTP error occurred: {http_err}")
+    except Timeout as timeout_err:
+        logger.error(f"Request timed out: {timeout_err}")
+    except RequestException as req_err:
+        logger.error(f"Request exception: {req_err}")
+    except Exception as err:
+        logger.error(f"An error occurred: {err}")
+
+    return None  # Return None if there was an error
