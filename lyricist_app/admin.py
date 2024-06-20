e31e4f0ad175a190ea98897fdc18890c7298a374
ketandev <devketanpro11@gmail.com>
2024-06-20 11:31:24 +0530

    register Song and Country models in admin

--- lyricist_app/admin.py
+++ lyricist_app/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
-# Register your models here.
+from lyricist_app.models import Country, Song
+
+@admin.register(Song)
+class SongAdmin(admin.ModelAdmin):
+    list_display = ('artist', 'title', 'summary')
+    search_fields = ('artist', 'title')
+    filter_horizontal = ('countries',)
+
+@admin.register(Country)
+class CountryAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    search_fields = ('name',)
